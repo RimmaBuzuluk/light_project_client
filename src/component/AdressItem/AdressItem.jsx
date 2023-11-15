96a0b55534f664733be6d9247bec8ba21e02e96b
RimmaBuzuluk <rimmysya2000@gmail.com>
2023-11-15 14:10:21 +0200

    adress

---
+++ src/component/AdressItem/AdressItem.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./AdressItem.css";
+import { Avatar, Button, Paper, Typography } from "@mui/material";
+
+export const AdressItem = () => {
+  const status = true; // Припустиме значення, яке залежить від вашого додатку
+
+  return (
+    <Paper className="AdressItemContainer">
+      <div className="name_adress">name</div>
+      <div className="status_block">
+        
+      </div>
+    </Paper>
+  );
+};
