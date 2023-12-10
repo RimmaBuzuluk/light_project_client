cd7dd1d6410d34b4b3e85205351ba0c00fdedbdf
RimmaBuzuluk <rimmysya2000@gmail.com>
2023-12-10 16:53:22 +0200

    adress items skeleton

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -4,21 +4,25 @@ import axios from '../axios';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchAdress } from '../redux/slice/adressReduser';
 // import {addAressAction} from "../redux/slice/adressReduser"
-// import AdressItem from '../component/AdressItem';
+import AdressItem from '../component/AdressItem';
+import AdressItemSkeleton from '../skeleton/AdressItemSkeleton';
 
 
 function Home() {
   const dispatch=useDispatch()
   const {adress}=useSelector(state=>state.adress)
+  // console.log(adress)
 
-  const isAdressLoading =adress.status==='loading'
+  const isAdressLoading = adress.status==='loading'
+  const adressItem=adress.items
+  console.log(adressItem.length)
 
   React.useEffect(()=>{
     dispatch(fetchAdress())
   },[])
 
 
-  console.log(isAdressLoading, adress.status)
+  // console.log(isAdressLoading, adress.status)
 
   // const addAdress=()=>{
   //   const newAdress={
@@ -35,16 +39,23 @@ function Home() {
     <div className='home'>
         <div className='home__name'>имя и фамилия</div>
         <div className='home__addBut'>
-          {/* <button className="buttonAdd button" onClick={()=>addAdress() }> add adress</button> */}
+          <button className="buttonAdd button" 
+          // onClick={()=>addAdress() }
+          > 
+          add adress
+          </button>
         </div>
-        {/* <div className='adressBlocks'>
-          {adress.length>0 ? 
+        <div className='adressBlocks'>
+          {isAdressLoading ? <div><AdressItemSkeleton/></div>:
           <div>
-            {adress.map(adressSingle=><AdressItem key={adressSingle.id_adress} adressInform={adressSingle}/>)}
-          </div>
-          :
-          <div>Ви не маєте жодної адреси</div>}
-        </div> */}
+            {adressItem.length>0 ? 
+            <div>
+              {adressItem.map(adressSingle=><AdressItem key={adressSingle.id_adress} adressInform={adressSingle}/>)}
+            </div>
+            :
+            <div>Ви не маєте жодної адреси</div>}</div>
+          }
+        </div>
     </div>
   );
 }
