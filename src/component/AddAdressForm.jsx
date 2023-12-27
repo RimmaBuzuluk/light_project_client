947c25e59e787d0cbb85e336698cf3d0bed2abd3
RimmaBuzuluk <rimmysya2000@gmail.com>
2023-12-27 22:18:02 +0200

    addAdressForm style

--- src/component/AddAdressForm.jsx
+++ src/component/AddAdressForm.jsx
@@ -10,7 +10,17 @@ function AddAdressForm() {
 
     return (
         <div className='addAdressForm'>
-
+            <div className="addAdressFormTitle">Add new adress</div>
+            <div className="input_block">
+                <div className="input_adress">
+                    <div className="title_input_adress title_adress">adress</div>
+                    <input type="text" />
+                </div>
+                <div className="input_adress" >
+                    <div className="title_input_adress_place title_adress">adress place</div>
+                    <input type="text" />
+                </div>
+            </div>
         </div>
     );
 }
