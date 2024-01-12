30e2940d265de2c7f6f033da3da03badf4fa3658
RimmaBuzuluk <rimmysya2000@gmail.com>
2024-01-12 15:49:18 +0200

    city list, filter,

--- src/component/AddAdressForm.jsx
+++ src/component/AddAdressForm.jsx
@@ -1,53 +1,42 @@
+// AddAdressForm.jsx
+
 import React, { useEffect, useState } from 'react';
 import "../style/AddAdressForm.css";
 import { useDispatch, useSelector } from 'react-redux';
 import { selectIsAuth } from '../redux/slice/authReduser';
 import axios from '../axios';
-import { InputLabel, MenuItem, Select } from '@mui/material';
-import { fetchCity } from '../redux/slice/cityReduser';
-import { FixedSizeList as List } from 'react-window';
-
-
+import SerchCity from './SerchCity';
 
 function AddAdressForm({onSubmit}) {
-    const dispatch=useDispatch()
-    const isAuth=useSelector(selectIsAuth)
-    const [address, setAddress] = useState(""); // Поміняв назву стейту на setContentAdress
+    const dispatch = useDispatch();
+    const isAuth = useSelector(selectIsAuth);
+    const [address, setAddress] = useState("");
     const [addressPlace, setAddressPlace] = useState("");
-    const [isLoading, setIsLoading]=useState(false)
-    const [selectedCity, setSelectedCity]=useState("")
+    const [isLoading, setIsLoading] = useState(false);
+    const [selectedCity, setSelectedCity] = useState("");
 
-    const {items:CityData, status:CityStatus} = useSelector(state => state.city);
+    const handleSelectCity = (cityName) => {
+        setSelectedCity(cityName);
+    };
 
-    useEffect(()=>{
-        dispatch(fetchCity())
-      },[])
-
-    const isCityLoading = CityStatus.status==='loading'
-
-    // const city=CityData[0].name
-    // console.log('CityData:', city);//чернигів
-
-
-
-    const onSubmitAdd=async()=>{
-        try{
-            setIsLoading(true)
-            const addressParam={
+    const onSubmitAdd = async () => {
+        try {
+            setIsLoading(true);
+            const addressParam = {
                 address,
                 addressPlace,
-                city: selectedCity
-            }
+                city: selectedCity,
+            };
 
-            const {data}=await axios.post(`/address`,addressParam)
-            onSubmit()
-        }catch(err){
-            console.warn(err)
-            alert('Помилка при створенні адреси')
+            const { data } = await axios.post(`/address`, addressParam);
+            onSubmit();
+        } catch (err) {
+            console.warn(err);
+            alert('Помилка при створенні адреси');
         }
-    }
+    };
 
-    
+    console.log("sele",selectedCity)
 
     return (
         <div className='addAdressForm'>
@@ -61,32 +50,8 @@ function AddAdressForm({onSubmit}) {
                     <div className="title_input_adress_place title_adress">Address place</div>
                     <input type="text" value={addressPlace} name="addressPlace" onChange={(e) => setAddressPlace(e.target.value)} />
                 </div>
-                <div className="input_adress" >
-                    <InputLabel className="title_input_adress_place title_adress">City</InputLabel>
-                    <Select type="text" value={selectedCity} name="city" onChange={(e) => setSelectedCity(e.target.value)}>
-                    {/* Відображення елементів списку міст */}
-                    {isCityLoading
-                        ? <div>Завантаження...</div>
-                        : (
-                            CityData
-                       ?
-                          <List
-                            height={200}
-                            itemCount={CityData.length}
-                            itemSize={35}
-                            width={300}
-                          >
-                            {({ index, style }) => (
-                                        <MenuItem key={CityData[index]._id} value={CityData[index].name} style={style}>
-                                            {CityData[index].name}
-                                        </MenuItem>
-                                    )}
-                          </List>
-                       :
-                       <div>no</div>
-                        )}
-                    </Select>
-                </div>
+                {/* Передача функції handleSelectCity як пропс в компонент SerchCity */}
+                <SerchCity onSelectCity={handleSelectCity} setSelectedCity={setSelectedCity} />
             </div>
             <button className='buttonAddAdress' onClick={onSubmitAdd}>Add New Address</button>
         </div>
---
+++ src/component/SerchCity.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import "../style/SerchCity.css";
+import { FixedSizeList as List } from 'react-window';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchCity } from '../redux/slice/cityReduser';
+
+function SerchCity({ onSelectCity}) {
+    const dispatch = useDispatch();
+    const [filterText, setFilterText] = useState("");
+    const { items: CityData, status: CityStatus } = useSelector(state => state.city);
+
+    useEffect(() => {
+        dispatch(fetchCity());
+    }, []);
+
+    const isCityLoading = CityStatus.status === 'loading';
+
+    const filteredCities = CityData.filter(city => 
+        city.name && city.name.toLowerCase().includes(filterText.toLowerCase())
+    );
+
+    
+    const handleSelectCity = (cityName) => {
+        console.log(cityName)
+        setFilterText(cityName)
+        onSelectCity(filterText)
+        
+    };
+
+    return (
+        <div className='SerchCity input_adress'>  
+            <div className="title_input_adress_place title_adress">City</div>
+            <div>
+                <input type="text" value={filterText} name="addressPlace" onChange={(e) => setFilterText(e.target.value)} />
+                {isCityLoading
+                ? <div>Завантаження...</div> 
+                : (CityData
+                    ? (filteredCities.length > 0 ? (
+                    <List
+                        height={200}
+                        itemCount={filteredCities.length}
+                        itemSize={35}
+                        width={300}
+                    >
+                        {({ index, style }) => (
+                            <button onClick={() => handleSelectCity(filteredCities[index].name)} key={filteredCities[index]._id} value={filteredCities[index].name} style={style}>
+                                {filteredCities[index].name}
+                            </button>
+                        )}
+                    </List>
+                    ) : <div>No matching cities</div>)
+
+                    : <div>Завантаження...</div>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default SerchCity;
