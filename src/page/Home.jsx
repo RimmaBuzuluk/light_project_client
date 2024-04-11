799c14ceab7e6b37f04db0657991d15ceb50e25c
RimmaBuzuluk <rimmysya2000@gmail.com>
2024-04-11 12:49:39 +0300

    address item item

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import "../style/Home.css"
+import '../style/Home.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchAddress } from '../redux/slice/adressReduser';
 import HomeSkeleton from '../skeleton/HomeSkeleton';
@@ -8,50 +8,65 @@ import AdressItem from '../component/AdressItem';
 import AdressItemSkeleton from '../skeleton/AdressItemSkeleton';
 import AddAdressForm from '../component/AddAdressForm';
 
-
 function Home() {
-  const dispatch=useDispatch()
-  const {data:userData, status:userStatus} = useSelector(state => state.auth);
-  const addressData = useSelector(state => state.address);
-  const [addressForm, setAddressForm]=useState(false)
+	const dispatch = useDispatch();
+	const { data: userData, status: userStatus } = useSelector(state => state.auth);
+	const addressData = useSelector(state => state.address);
+	const [addressForm, setAddressForm] = useState(false);
 
-  useEffect(()=>{
-    dispatch(fetchAddress())
-  },[])
+	useEffect(() => {
+		dispatch(fetchAddress());
+	}, []);
 
-  const isAuth=useSelector(selectIsAuth)
-  const isUserLoading = userStatus==='loading'
-  const isAdressLoading = addressData.status==='loading'
+	const isAuth = useSelector(selectIsAuth);
+	const isUserLoading = userStatus === 'loading';
+	const isAdressLoading = addressData.status === 'loading';
 
-  if (isUserLoading || !isAuth){
-    return(<HomeSkeleton/>)
-  }
+	if (isUserLoading || !isAuth) {
+		return <HomeSkeleton />;
+	}
 
-  const onSubmit=()=>{
-    console.log(addressForm)
-    setAddressForm(!addressForm)
-  }
+	const onSubmit = () => {
+		console.log(addressForm);
+		setAddressForm(!addressForm);
+	};
 
-  return (
-    <div className='home'>
-      <div className='home__name'>{userData.fullName}</div>
-        <div className='home__addBut'>
-          <button onClick={onSubmit} className="buttonAdd button">Add address</button>
-        </div>
-        {addressForm ? <div className='formAddAdress'><AddAdressForm onSubmit={onSubmit}/></div>:<div></div>}
-        <div className='adressBlocks'>
-          {isAdressLoading? <div><AdressItemSkeleton/></div>:
-            <div>
-              {addressData.items.length>0 ? <div>
-                {addressData.items.map(address=><AdressItem key={address._id} address={address}/>)}
-              </div>
-              :
-              <div>Ви не маєте жодної адреси</div>}
-            </div>
-          }
-        </div> 
-    </div>
-  );
+	return (
+		<div className='home'>
+			<div className='home__name'>powered by {userData.fullName}</div>
+			<div className='home__addBut'>
+				<button onClick={onSubmit} className='buttonAdd button'>
+					Add address
+				</button>
+			</div>
+			{addressForm ? (
+				<div className='formAddAdress'>
+					<AddAdressForm onSubmit={onSubmit} />
+				</div>
+			) : (
+				<div></div>
+			)}
+			<div className='adressBlocks'>
+				{isAdressLoading ? (
+					<div>
+						<AdressItemSkeleton />
+					</div>
+				) : (
+					<div>
+						{addressData.items.length > 0 ? (
+							<div>
+								{addressData.items.map(address => (
+									<AdressItem key={address._id} address={address} />
+								))}
+							</div>
+						) : (
+							<div>Ви не маєте жодної адреси</div>
+						)}
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default Home;
